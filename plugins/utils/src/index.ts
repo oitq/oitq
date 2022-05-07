e54374b95f32e70651fea47d0fdd89c33949acd1
Administrator <admin@liucl.cn>
2022-05-08 07:40:02 +0800

    axios.load utils

--- plugins/utils/src/index.ts
+++ plugins/utils/src/index.ts
@@ -1,7 +1,8 @@
 import {Context} from "oitq";
 import {Requester} from "./request/requester";
 import * as time from './time'
-import axios from "axios";
+import {MessageElem, segment} from "oicq";
+import {toCqcode} from "@oitq/utils";
 declare module 'oitq'{
     namespace Context{
         interface Services{
@@ -37,6 +38,27 @@ export function install(ctx:Context,config:Config){
                 return JSON.stringify(res,null,2)
             }catch {
                 return typeof res==='string'?res:undefined
+            }
+        })
+    cmd.subcommand('axios.load <url>','加载资源')
+        .option('type','-t [type] 资源类型，可选值：image,music,video，默认：image',{fallback:'image'})
+        .action(async ({session,options},url)=>{
+            const res=await ctx.axios.get(url,{
+                responseType: 'arraybuffer'
+            })
+            try{
+                switch (options.type){
+                    case 'music':
+                        return toCqcode({message:[segment.record(`base64://${Buffer.from(res,'binary').toString('base64')}`)]})
+                    case 'image':
+                        return toCqcode({message:[segment.image(`base64://${Buffer.from(res,'binary').toString('base64')}`)]})
+                    case 'video':
+
+                    default:
+                        return '未知类型：'+options.type
+                }
+            }catch (e){
+                return e.message
             }
         })
     cmd.subcommand('axios.post <url>','发起post请求')
